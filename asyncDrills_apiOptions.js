993f17bb0e2d25b152f5a02678327daf2533e3f8
mmanueljoe <emmanuel.letsu@amalitech.com>
2026-03-02 09:53:05 +0000

    feat: add delayed fetches and api options

--- asyncDrills_apiOptions.js
+++ asyncDrills_apiOptions.js
@@ -19,7 +19,29 @@ const BASE_URL = 'https://jsonplaceholder.typicode.com';
 // Also: catch(error) — not catch { } if you use error in the block.
 
 async function fetchWithTimeout(url, timeoutMs = 3000) {
-    // TODO
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), timeoutMs);
+
+    try{
+        const response = await fetch(url, {
+            signal: controller.signal
+        })
+
+        if(!response.ok){
+            throw new Error(`Request failed with status: ${response.status}`)
+        }
+
+        const data = response.json();
+
+        return data;
+    }catch(error){
+        console.log(`Error fetching data: ${error}`);
+        throw error;
+    }finally{
+        
+        clearTimeout(timeoutId);
+    }
+    
 }
 
 // -----------------------------------------------------------------------------
@@ -31,7 +53,28 @@ async function fetchWithTimeout(url, timeoutMs = 3000) {
 // Optional: also add method: 'GET' (or 'POST' if you're feeling ambitious)
 
 async function fetchWithCustomHeaders(url, extraHeaders = {}) {
-    // TODO
+    try{
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Custom': 'hello',
+                ...extraHeaders
+            }
+        });
+
+        if(!response.ok){
+            throw new Error(`Request failed with status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        return data
+
+    }catch(error){
+        console.log(`Error fetching data: ${error}`);
+        throw error;
+    }
 }
 
 // -----------------------------------------------------------------------------
@@ -41,6 +84,25 @@ async function fetchWithCustomHeaders(url, extraHeaders = {}) {
 // retry up to 2 times with 1s delay between tries.
 // Reuse your fetchWithTimeout. Wrap it in retry logic.
 
+function wait(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function fetchWithTimeoutAndRetry(url, timeoutMs = 2000, maxRetries = 2) {
-    // TODO
+   let attempt = 0;
+
+   while(attempt <= maxRetries){
+    try{
+        const data = await fetchWithTimeout(url, timeoutMs);
+
+        return data;
+    }catch(error){
+        if(attempt === maxRetries){
+            throw error;
+        }
+
+        attempt += 1;
+        await wait(1000)
+    }
+   }
 }
